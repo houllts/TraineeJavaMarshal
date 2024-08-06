7fa272c15cfc04062a793a42caeb5a184308fa27
Elena <houllts@gmail.com>
2024-08-06 13:54:43 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -27,3 +27,27 @@ public class Main {
     }
 
 }
+
+//Умножение
+public class Main {
+    public static void main (String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+
+        int result = num1 * num2;
+
+        System.out.println("Результат: " + result);
+    }
+}
+
+//Деление
+public class Main {
+    public static void main (String[] args) {
+        int num1 = 16;
+        int num2 = 4;
+
+        int result = num1 / num2;
+
+        System.out.println("Результат: " + result);
+    }
+}
